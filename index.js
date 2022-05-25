7000df449b401df52cb492d027af02584bf83d9e
PeterTheOne <petertheone@gmail.com>
2022-05-25 13:29:55 +0200

    ignore new links for first 10 fetches, add ignore and highlight words

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ const INTERVAL = process.env.INTERVAL;
 const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN; // bot from https://t.me/botfather
 const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID; // id from https://telegram.me/userinfobot
 const XPATH_QUERY = process.env.XPATH_QUERY;
+const HIGHLIGHT_WORDS = process.env.HIGHLIGHT_WORDS.split(',');
+const IGNORE_WORDS = process.env.IGNORE_WORDS.split(',');
 
 const telegramMessagePath = `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage?chat_id=${TELEGRAM_CHAT_ID}&text=`;
 
-async function fetchLinks(url, query) {
+async function fetchLinks(url, query, highlightWords, ignoreWords) {
   const result = await fetch(url);
   const html = await result.text();
   const document = parse5.parse(html.toString());
@@ -22,26 +24,45 @@ async function fetchLinks(url, query) {
   const select = xpath.useNamespaces({"x": "http://www.w3.org/1999/xhtml"});
   const nodes = select(query, doc);
   const willhabenURL = new URL(WILLHABEN_URL);
-  return nodes.map((node) => {
+  return nodes
+    .map((node) => {
     const url = new URL(willhabenURL.protocol + '//' + willhabenURL.host + node.value);
-    return url.origin + url.pathname;
+    return {
+      link: url.origin + url.pathname,
+      ignore: ignoreWords.some(element => node.value.includes(element)),
+      foundHighlightWords: highlightWords.filter(h => node.value.includes(h)),
+    };
   });
 }
 
 async function start() {
-  const linkCache = await fetchLinks(WILLHABEN_URL, XPATH_QUERY);
+  const linkCache = [];
+  let countFetches = 0;
 
-  setInterval(async () => {
-    console.log('check for new links');
-    const newLinks = await fetchLinks(WILLHABEN_URL, XPATH_QUERY);
-    newLinks.forEach((newLink) => {
-      if (!linkCache.includes(newLink)) {
-        linkCache.push(newLink);
-        fetch(telegramMessagePath + encodeURI(newLink));
-        console.log('new', newLink);
-      }
-    });
-  }, INTERVAL);
+  const loop = async () => {
+    try {
+      console.log('check for new links', countFetches);
+      const newResults = await fetchLinks(WILLHABEN_URL, XPATH_QUERY, HIGHLIGHT_WORDS, IGNORE_WORDS);
+      newResults.forEach((r) => {
+        if (!linkCache.includes(r.link)) {
+          linkCache.push(r.link);
+          const foundHighlightWordStr = r.foundHighlightWords.map(h => `#${h}`.replace('-', '')).join(' ');
+          //console.log('new', r.link, r.ignore, foundHighlightWordStr);
+          // Don't message for the first 10 fetches.
+          if (countFetches > 10 && !r.ignore) {
+            fetch(telegramMessagePath + encodeURI(`${r.link} ${foundHighlightWordStr}`));
+            console.log('new', r.link, foundHighlightWordStr);
+          }
+        }
+      });
+      countFetches++;
+    } catch (e) {
+      console.error(e);
+    }
+    setTimeout(loop, INTERVAL);
+  }
+
+  loop();
 }
 
 start();
